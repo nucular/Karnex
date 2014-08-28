b9f8a1675c7a743473f5769d8dd9984a4fc89ffd
Ian Sannar <ian.sannar@gmail.com>
2014-08-28 16:52:30 -0600

    Add files
    Artwork, Logo and the skeleton of code.

---
+++ Karnex.h
@@ -0,0 +1,18 @@
+﻿/*
+* Karnex.h - <insert interesting description for this header file>
+*
+* <Insert copyright stuff here>
+*
+* <Insert GNU stuff here>
+*/
+
+
+#ifndef _KARNEX_H
+#define _KARNEX_H
+
+
+#include Instrument.h
+#include InstrumentView.h
+#include Oscillator.h
+#include Knob.h
+//#include (Oscillator Chooser thang, whether it be buttons or whatever)
